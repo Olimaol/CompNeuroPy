000f41839411fa19ed5283ff4c020e7004e1df53
olimaol <oli_maith@gmx.de>
2022-05-09 17:03:18 +0200

    added combine_data funcitons to recording_times class of Monitors class
    renamed opt_izh in opt_neuron
    
    added start_time and end_time to the current_pulse simulation function

--- CompNeuroPy/Monitors.py
+++ CompNeuroPy/Monitors.py
@@ -2,6 +2,7 @@ import CompNeuroPy.neuron_models as nm
 import CompNeuroPy.model_functions as mf
 import CompNeuroPy.simulation_functions as sif
 import CompNeuroPy.system_functions as syf
+import CompNeuroPy.extra_functions as ef
 from ANNarchy import get_time, dt, reset
 import numpy as np
 
@@ -85,22 +86,113 @@ class recording_times_cl:
     def __init__(self, recording_times_list):
         self.recording_times_list = recording_times_list
         
-    def time_lims(self, chunk=0, compartment=None, period=0):
-        if compartment==None:
-            ### by default just use the first compartment
-            compartment=list(self.recording_times_list[chunk].keys())[0]
-        time_lims = [self.recording_times_list[chunk][compartment]['start']['ms'][period], self.recording_times_list[chunk][compartment]['stop']['ms'][period]]
-        return time_lims
+    def time_lims(self, chunk=None, compartment=None, period=None):
+        return self.__lims__('ms', chunk, compartment, period)
         
-    def idx_lims(self, chunk=0, compartment=None, period=0):
-        if compartment==None:
-            ### by default just use the first compartment
-            compartment=list(self.recording_times_list[chunk].keys())[0]
-        idx_lims = [self.recording_times_list[chunk][compartment]['start']['idx'][period], self.recording_times_list[chunk][compartment]['stop']['idx'][period]]
-        return idx_lims
+    def idx_lims(self, chunk=None, compartment=None, period=None):
+        return self.__lims__('idx', chunk, compartment, period)
         
     def all(self):
         return self.recording_times_list
+        
+    def combine_data(self, recordings, recording_data_str, mode='sequential'):
+        """
+            recordings: recordings array of recording chunks
+            recording_data_str: str of compartment + recorded variable
+            mode: how should the time array be generated
+                sequential: each chunk starts at zero e.g.: [0,100] + [0,250] --> [0, 1, ..., 100, 0, 1, ..., 250]
+                consecutive: each chunk starts at the last stop time of the previous chunk e.g.: [0,100] + [0,250] --> [0, 1, ..., 100, 101, 102, ..., 350]
+        """
+        compartment = recording_data_str.split(';')[0]
+        dt = recordings[0]['dt']
+        nr_chunks = self.__get_nr_chunks__()
+        data_list = []
+        time_list = []
+        pre_chunk_start_time = 0
+        for chunk in range(nr_chunks):
+            ### append data list with data of all periods of this chunk
+            data_list.append(recordings[chunk][recording_data_str])
+            
+            ### nr of periods in this chunk
+            nr_periods = self.__get_nr_periods__(chunk, compartment)
+            
+            ### start time of chunk depends on mode
+            if mode == 'sequential':
+                chunk_start_time = 0
+            elif mode == 'consecutive':
+                if chunk == 0:
+                    chunk_start_time = 0
+                else:
+                    last_stop_time = self.recording_times_list[chunk-1][compartment]['stop']['ms'][nr_periods-1]
+                    chunk_start_time = pre_chunk_start_time + last_stop_time
+                    pre_chunk_start_time = chunk_start_time
+            else:
+                print('ERROR recording_times.combine_data, Wrong mode.')
+                quit()
+            
+            ### append the time list with all times of the periods
+            for period in range(nr_periods):
+                start_time = self.recording_times_list[chunk][compartment]['start']['ms'][period] + chunk_start_time
+                end_time   = self.recording_times_list[chunk][compartment]['stop']['ms'][period] + chunk_start_time
+                times      = np.arange(start_time, end_time, dt)
+                time_list.append(times)
+
+        ### flatten the two lists
+        data_arr = np.array(ef.flatten_list(data_list))
+        time_arr = np.array(ef.flatten_list(time_list))
+        
+        return [time_arr, data_arr]
+        
+    def __lims__(self, string, chunk=None, compartment=None, period=None):
+        chunk              = self.__check_chunk__(chunk)
+        compartment        = self.__check_compartment__(compartment, chunk)
+        period_0, period_1 = self.__check_period__(period, chunk, compartment) 
+        lims = [self.recording_times_list[chunk][compartment]['start'][string][period_0], self.recording_times_list[chunk][compartment]['stop'][string][period_1]]
+        return lims
+        
+    def __check_compartment__(self, compartment, chunk):
+        if compartment==None:
+            ### by default just use the first compartment
+            compartment=list(self.recording_times_list[chunk].keys())[0]
+        elif compartment in list(self.recording_times_list[chunk].keys()):
+            compartment=compartment
+        else:
+            print('ERROR recording_times, given compartment "'+str(compartment)+'" not available')
+            quit()
+            
+        return compartment
+        
+    def __check_period__(self, period, chunk, compartment):
+        if period==None:
+            ### by default use all periods
+            period_0 = 0
+            period_1 = len(self.recording_times_list[chunk][compartment]['start']['idx'])-1
+        elif period<len(self.recording_times_list[chunk][compartment]['start']['idx']):
+            period_0 = period
+            period_1 = period
+        else:
+            print('ERROR recording_times, given period not available')
+            quit()
+            
+        return [period_0, period_1]
+        
+    def __check_chunk__(self, chunk):
+        if chunk==None:
+            ### by default use all periods
+            chunk = 0
+        elif chunk<self.__get_nr_chunks__():
+            chunk = chunk
+        else:
+            print('ERROR recording_times, given chunk not available')
+            quit()
+            
+        return chunk
+        
+    def __get_nr_chunks__(self):
+        return len(self.recording_times_list)
+        
+    def __get_nr_periods__(self, chunk, compartment):
+        return len(self.recording_times_list[chunk][compartment]['start']['idx'])
         
         
         
--- CompNeuroPy/extra_functions.py
+++ CompNeuroPy/extra_functions.py
@@ -6,3 +6,10 @@ def print_df(df):
     """
     with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
         print(df)
+        
+def flatten_list(lst):
+    """
+        lst: list of lists
+        retuns flattened list
+    """
+    return [item for sublist in lst for item in sublist]
