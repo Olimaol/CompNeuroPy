441896bca4016fcbf7ef1f13382649b9cab66a5d
olimaol <oli_maith@gmx.de>
2022-05-24 13:58:44 +0200

    updated setup version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ dependencies = [
 
 setuptools.setup(
     name="CompNeuroPy",
-    version="0.0.2",
+    version="0.0.3",
     license="MIT",
     author="Oliver Maith",
     author_email="oli_maith@gmx.de",
