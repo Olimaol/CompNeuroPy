197fc97932ef08a861a7aaeacb0ff0aa31d1a75d
olmai <oli_maith@gmx.de>
2022-09-27 11:19:19 +0200

    updated setup version

--- setup.py
+++ setup.py
@@ -6,26 +6,26 @@ except:
     assert False
 
 dependencies = [
-    'numpy',
-    'scipy',
-    'matplotlib',
-    'cython',
-    'sympy',
-    'hyperopt',
-    'ANNarchy',
-    'pandas',
-    'sbi',
-    'torch'
+    "numpy",
+    "scipy",
+    "matplotlib",
+    "cython",
+    "sympy",
+    "hyperopt",
+    "ANNarchy",
+    "pandas",
+    "sbi",
+    "torch",
 ]
 
 setuptools.setup(
     name="CompNeuroPy",
-    version="0.0.7",
+    version="0.0.8",
     license="MIT",
     author="Oliver Maith",
     author_email="oli_maith@gmx.de",
     description="General package for computational neuroscience with ANNarchy.",
     url="https://github.com/Olimaol/compneuropy",
     packages=setuptools.find_packages(),
-    install_requires=dependencies
+    install_requires=dependencies,
 )
