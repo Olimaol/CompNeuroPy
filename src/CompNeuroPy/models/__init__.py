6a7d74d637c7546ca1d93ee18d3763ab339b3bd2
olmai <oli_maith@gmx.de>
2023-12-07 16:14:08 +0100

    initial commit for site

--- src/CompNeuroPy/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .H_and_H_model_Bischop import H_and_H_model_Bischop
-from .H_and_H_model_Corbit import H_and_H_model_Corbit
-from .H_and_H_model_Corbit_syn import H_and_H_model_Corbit_syn
-from .BGM_22.BGM import BGM
